fc95cf9dbf7a585c656c8b1330eeb7c7b8fd44cf
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-02-24 15:50:03 +0300

    hero finished

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css";
+import hero from "../../assets/hero6.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero-container">
+      <div className="hero-image-div">
+        <img alt="hero-image" className="hero-image" src={hero} />
+        <div className="hero-container-inside-left">
+          <h1 className="hero-title">Welcome to PulsePing</h1>
+          <h2 className="hero-subtitle">
+            Stay ahead of outages, make your API stay awake.
+          </h2>
+        </div>
+        <div className="hero-container-inside-right">
+          <a href="/dashboard">Go to dashboard.</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
